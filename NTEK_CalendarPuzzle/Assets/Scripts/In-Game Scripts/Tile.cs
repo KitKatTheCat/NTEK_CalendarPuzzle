c75b3f3873eac98e0fb514c0935b5de3bc07e995
Jan Adam K. Berdan <jakberdan312@gmail.com>
2023-07-26 17:50:14 +0800

    - Added Pieces and Board with functionalities
    - Added a snap System to the board
    - Added a custom grid for the board
    - Added Selection to the pieces
    - Added Dragging and returning it back if invalid
    - Added Rotation to the pieces

---
+++ NTEK_CalendarPuzzle/Assets/Scripts/In-Game Scripts/Tile.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public bool isOccupied;
+
+    public Color occupiedColor;
+    public Color vacantColor;
+}
